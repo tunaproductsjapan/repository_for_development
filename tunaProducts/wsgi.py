cafda24d1fe92fa7df2c4bcf7d274f6888074778
hiroaki415 <hiroaki415@gmail.com>
2024-09-25 00:57:40 +0900

    change a bit of wsgi.py

--- tunaProducts/wsgi.py
+++ tunaProducts/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys #add for local
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(r'C:\Users\hiroa\Documents\tunaProducts') #add for local
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tunaProducts.settings')
 
 application = get_wsgi_application()
